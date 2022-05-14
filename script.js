160848a35280b0f84acb0668dcd634b1e18013c6
Pande <popovski.pande@gmail.com>
2022-05-14 15:44:28 +0200

    Edit playRound function in script.js
    Edited the playRound function so it could have all outcomes.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//function for random computer selection
 let computerSelection;
 function computerPlay(computerSelection) {
     outputs = ["rock", "paper", "scissors"];
@@ -5,12 +6,28 @@ function computerPlay(computerSelection) {
     return computerSelection;
 }
 
+//function for playing one round - NEED TO MAKE FUNCTION BRING UP PROMPT FOR PLAYERSELECT
 let playerSelection;
 let result;
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return result = `Tie! You both chose ${playerSelection}`;
+    playerSel = playerSelection.toLowerCase();
+    computerSel = computerSelection.toLowerCase();
+    if ((playerSelection === "scissors") && (computerSel === "paper")) {
+        return result = `You win! Scissors beats Paper.`;
+    } else if ((playerSel === "rock") && (computerSel === "paper")) {
+        return result = `You lose! Paper beats Rock.`;
+    } else if ((playerSel === "rock") && (computerSel === "scissors")) {
+        return result = `You win! Rock beats Scissors.`;
+    } else if ((playerSel === "paper") && (computerSel === "scissors")) {
+        return result = `You lose! Scissors beats Paper.`;
+    } else if ((playerSel === "paper") && (computerSel === "rock")) {
+        return result = `You win! Paper beats Rock.`;
+    } else if ((playerSel === "scissors") && (computerSel === "rock")) {
+        return result = `You lose! Rock beats Scissors.`;
     } else {
-        return result = 'da dopram';
+        selection = playerSel.slice(0,1).toUpperCase() + playerSel.slice(1);
+        return result = `Tie! You both chose ${selection}.`;
     }
 }
+
+//
