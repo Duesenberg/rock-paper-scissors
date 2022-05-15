ab085d118bdb7804ba9377a4a3b8bbb0c3daef77
Pande <popovski.pande@gmail.com>
2022-05-15 22:54:45 +0200

    Make small changes to script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 //function for random computer selection
 let computerSelection;
-function computerPlay(computerSelection) {
-    outputs = ["rock", "paper", "scissors"];
+function computerPlay() {
+    let outputs = ["rock", "paper", "scissors"];
     computerSelection = outputs [Math.floor(Math.random()* outputs.length)];
-    return computerSelection;
 }
 
 //function for playing one round - NEED TO MAKE FUNCTION BRING UP PROMPT FOR PLAYERSELECT
@@ -11,18 +10,17 @@ let playerSelection;
 let result;
 function playRound(playerSelection, computerSelection) {
     playerSel = playerSelection.toLowerCase();
-    computerSel = computerSelection.toLowerCase();
-    if ((playerSelection === "scissors") && (computerSel === "paper")) {
+    if ((playerSelection === "scissors") && (computerSelection === "paper")) {
         return result = `You win! Scissors beats Paper.`;
-    } else if ((playerSel === "rock") && (computerSel === "paper")) {
+    } else if ((playerSel === "rock") && (computerSelection === "paper")) {
         return result = `You lose! Paper beats Rock.`;
-    } else if ((playerSel === "rock") && (computerSel === "scissors")) {
+    } else if ((playerSel === "rock") && (computerSelection === "scissors")) {
         return result = `You win! Rock beats Scissors.`;
-    } else if ((playerSel === "paper") && (computerSel === "scissors")) {
+    } else if ((playerSel === "paper") && (computerSelection === "scissors")) {
         return result = `You lose! Scissors beats Paper.`;
-    } else if ((playerSel === "paper") && (computerSel === "rock")) {
+    } else if ((playerSel === "paper") && (computerSelection === "rock")) {
         return result = `You win! Paper beats Rock.`;
-    } else if ((playerSel === "scissors") && (computerSel === "rock")) {
+    } else if ((playerSel === "scissors") && (computerSelection === "rock")) {
         return result = `You lose! Rock beats Scissors.`;
     } else {
         selection = playerSel.slice(0,1).toUpperCase() + playerSel.slice(1);
