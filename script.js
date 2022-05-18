e5fe7c1d34793101a3b28f080f5f6057467cd3fb
Pande <popovski.pande@gmail.com>
2022-05-18 11:01:39 +0200

    Touch up script.js
    Made the game work as intended. All that is left is to take care of
    the invalid input situations affecting the final result somehow.

--- script.js
+++ script.js
@@ -8,32 +8,32 @@ function computerPlay() {
     computerSelection = outputs [Math.floor(Math.random()* outputs.length)];
 }
 
-//function for playing one round - winCount & loseCount UNREACHABLE
-let result;
+//function for playing one round
 function playRound(playerSelection, computerSelection) {
-    let winCount;
     playerSel = playerSelection.toLowerCase();
     if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        return result = `You win! Scissors beats Paper.`;
+        result = `You win! Scissors beats Paper.`;
         winCount += 1;
     } else if ((playerSel === "rock") && (computerSelection === "paper")) {
-        return result = `You lose! Paper beats Rock.`;
+        result = `You lose! Paper beats Rock.`;
         loseCount += 1;
     } else if ((playerSel === "rock") && (computerSelection === "scissors")) {
-        return result = `You win! Rock beats Scissors.`;
+        result = `You win! Rock beats Scissors.`;
         winCount += 1;
     } else if ((playerSel === "paper") && (computerSelection === "scissors")) {
-        return result = `You lose! Scissors beats Paper.`;
+        result = `You lose! Scissors beats Paper.`;
         loseCount += 1;
     } else if ((playerSel === "paper") && (computerSelection === "rock")) {
-        return result = `You win! Paper beats Rock.`;
+        result = `You win! Paper beats Rock.`;
         winCount += 1;
     } else if ((playerSel === "scissors") && (computerSelection === "rock")) {
-        return result = `You lose! Rock beats Scissors.`;
+        result = `You lose! Rock beats Scissors.`;
         loseCount += 1;
-    } else {
+    } else if (playerSel === computerSelection) {
         selection = playerSel.slice(0,1).toUpperCase() + playerSel.slice(1);
-        return result = `Tie! You both chose ${selection}.`;
+        result = (`Tie! You both chose ${selection}.`);
+    } else {
+        result = (`Invalid input. Please choose either Rock, Paper or Scissors.`);
     }
 }
 
@@ -47,7 +47,11 @@ function game() {
     alert(`Result: Win: ${winCount} Lose: ${loseCount}`);
     if (winCount > loseCount) {
         alert('You Win!');
+    } else if (winCount === loseCount) {
+        alert ('Tie!');
     } else {
         alert ('You Lose!');
     }
+    winCount = 0;
+    loseCount = 0;
 }
