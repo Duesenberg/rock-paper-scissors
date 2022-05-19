26aa1126965e7bfa02e45e7eb384dd4271e95530
Pande <popovski.pande@gmail.com>
2022-05-19 10:41:37 +0200

    Add invalid input counter & prompt when load page
    Added function to ask user whether they want to play the
    game. Also added a counter for how many inputs have been
    invalid.

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let winCount = 0;
 let loseCount = 0;
+let nonValidInput = 0;
 let computerSelection;
 let playerSelection;
+
 //function for random computer selection
 function computerPlay() {
     let outputs = ["rock", "paper", "scissors"];
@@ -12,31 +14,33 @@ function computerPlay() {
 function playRound(playerSelection, computerSelection) {
     playerSel = playerSelection.toLowerCase();
     if ((playerSelection === "scissors") && (computerSelection === "paper")) {
-        result = `You win! Scissors beats Paper.`;
+        result = `Computer chooses Paper.\n\nYou win! Scissors beats Paper.`;
         winCount += 1;
     } else if ((playerSel === "rock") && (computerSelection === "paper")) {
-        result = `You lose! Paper beats Rock.`;
+        result = `Computer chooses Paper.\n\nYou lose! Paper beats Rock.`;
         loseCount += 1;
     } else if ((playerSel === "rock") && (computerSelection === "scissors")) {
-        result = `You win! Rock beats Scissors.`;
+        result = `Computer chooses Scissors.\n\nYou win! Rock beats Scissors.`;
         winCount += 1;
     } else if ((playerSel === "paper") && (computerSelection === "scissors")) {
-        result = `You lose! Scissors beats Paper.`;
+        result = `Computer chooses Scissors.\n\nYou lose! Scissors beats Paper.`;
         loseCount += 1;
     } else if ((playerSel === "paper") && (computerSelection === "rock")) {
-        result = `You win! Paper beats Rock.`;
+        result = `Computer chooses Rock.\n\nYou win! Paper beats Rock.`;
         winCount += 1;
     } else if ((playerSel === "scissors") && (computerSelection === "rock")) {
-        result = `You lose! Rock beats Scissors.`;
+        result = `Computer chooses Rock.\n\nYou lose! Rock beats Scissors.`;
         loseCount += 1;
     } else if (playerSel === computerSelection) {
         selection = playerSel.slice(0,1).toUpperCase() + playerSel.slice(1);
         result = (`Tie! You both chose ${selection}.`);
     } else {
         result = (`Invalid input. Please choose either Rock, Paper or Scissors.`);
+        nonValidInput += 1;
     }
 }
 
+//function for playing the whole game
 function game() {
     for (i=0; i < 5; i++) {
         playerSelection = prompt('Type Rock, Paper or Scissors:');
@@ -44,7 +48,7 @@ function game() {
         playRound(playerSelection, computerSelection);
         alert(result);
     }
-    alert(`Result: Win: ${winCount} Lose: ${loseCount}`);
+    alert(`Result: Win: ${winCount} Lose: ${loseCount} Invalid inputs: ${nonValidInput}`);
     if (winCount > loseCount) {
         alert('You Win!');
     } else if (winCount === loseCount) {
@@ -54,4 +58,20 @@ function game() {
     }
     winCount = 0;
     loseCount = 0;
+    if (confirm('Want to play again?') === true) {
+        game ();
+    } else {
+        alert ("Game Over!");
+    }
 }
+
+//function that brings up prompt for playing game
+function wantToPlay() {
+    if (confirm('Want to play Rock, Paper, Scissors?') === true) {
+        game ();
+    } else {
+        alert ("Wimp!");
+    }
+}
+
+wantToPlay();
